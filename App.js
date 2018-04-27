a6849d7b9d33403e0c389816b08f68d27236dc09
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2018-04-27 10:51:51 +0300

    iOS için push çalışır duruma getirildi

--- App.js
+++ App.js
@@ -1,58 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    Text,
+    View,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import FCM, {
+    FCMEvent,
+    WillPresentNotificationResult,
+    NotificationType,
+} from "react-native-fcm";
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    async componentWillMount() {
+        // herhangi bir bildirim etkileşiminde çalışır
+        FCM.on(FCMEvent.Notification, notif => {
+            console.log("Bildirim geldi", notif);
+
+            if(Platform.OS ==='ios' && notif._notificationType === NotificationType.WillPresent && !notif.local_notification){
+                // Bu bildirim yalnızca kullanıcı ön planda ise bildirimi göstermek isteyip istemediğinize karar vermektir.
+                // genellikle görmezden gelebilirsiniz. sadece göster/gösterme yapar.
+                notif.finish(WillPresentNotificationResult.All)
+                return;
+            }
+        });
+
+        // eğer token yenilenirse çalışır
+        FCM.on(FCMEvent.RefreshToken, token => {
+            console.log("TOKEN YENİLENDİ (refreshUnsubscribe)", token);
+        });
+
+        // Uygulamaya Push atmak için izin alıyoruz
+        try {
+            let result = await FCM.requestPermissions({badge: false, sound: true, alert: true});
+        } catch(e){
+            console.error(e);
+        }
+
+        // Firebase Cloud Message Token değerini getirir.
+        FCM.getFCMToken().then(token => {
+            console.log("TOKEN (getFCMToken)", token);
+        });
+
+        if(Platform.OS === 'ios') {
+            // Eğer APNS istiyorsanız isteğe bağlı APNS TOKEN
+            FCM.getAPNSToken().then(token => {
+                console.log("APNS TOKEN (getAPNSToken)", token);
+            });
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    React Native Firebase Push Notification Kurulumu Uygulaması.
+                    Daha fazla bilgi için: <Text style={{color: 'blue'}}>github.com/abdurrahmanekr/rnpushapp</Text>
+                </Text>
+            </View>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ ios/Pods/Target Support Files/Pods-RNPushApp/Pods-RNPushApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNPushApp : NSObject
+@end
+@implementation PodsDummy_Pods_RNPushApp
+@end
